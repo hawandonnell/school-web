55ab77d94ac42b179a40b6edc3bf2cdb85b33058
jack_ <jack_jonhson96@mail.ru>
2020-01-25 17:47:43 +0600

    asd

--- school_website/settings.py
+++ school_website/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 import django_heroku
-import pytz
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,7 +27,7 @@ SECRET_KEY = 'sadlsadkaspkdsadkasdpka0qi202'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '*.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
